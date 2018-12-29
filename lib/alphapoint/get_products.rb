e1dbf8061247657cc4baa7947cabedfdc673108c
Lucas Perez <lucascperez@gmail.com>
2018-12-29 04:26:54 -0200

    pequeno refactor

--- lib/alphapoint/get_products.rb
+++ lib/alphapoint/get_products.rb
@@ -14,39 +14,15 @@ module Alphapoint
     end
 
     def execute(ws, &block)
-      response = [] 
-      puts "lalalalla"
+      responses = []
       ws.send(@frame.to_json)
       ws.on :message do |event|
-        # puts "to aqui dentro do on_message: #{event.data}"
         products_json = JSON.parse(event.data)
         JSON.parse(products_json["o"]).each do |product_json|
-          # puts product_json
-          # {\"OMSId\":1,\"ProductId\":1,
-          # \"Product\":\"BTC\",
-          # \"ProductFullName\":\"Bitcoin\",
-          # \"ProductType\":\"CryptoCurrency\",
-          # \"DecimalPlaces\":8,
-          # \"TickSize\":0.00000001,
-          # \"NoFees\":false}
-          response << GetProductsResponse.new(
-            product_json["OMSId"],
-            product_json["Product"],
-            product_json["ProductFullName"],
-            product_json["ProductType"],
-            product_json["DecimalPlaces"],
-            product_json["TickSize"],
-            product_json["NoFees"]
-          )
+          responses << GetProductsResponse.new(product_json["OMSId"],product_json["Product"],product_json["ProductFullName"],product_json["ProductType"],product_json["DecimalPlaces"],product_json["TickSize"],product_json["NoFees"])
         end
-        block.call(response)
+        block.call(responses)
       end
-    end
-
-    def handle_response(response)
-      return {
-        "a": 1
-      }
     end
   end
   
