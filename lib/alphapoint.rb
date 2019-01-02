6e597f8d9a8a16c59992c156c652780f7deb3cce
Fabiano Martins <fabiano.paula.martins@gmail.com>
2019-01-02 15:07:09 -0200

    removing useless requires

--- lib/alphapoint.rb
+++ lib/alphapoint.rb
@@ -4,17 +4,9 @@ require 'eventmachine'
 require 'yaml'
 require 'active_support/all'
 
-
 require 'alphapoint/configuration'
 require 'alphapoint/web_socket'
-require 'alphapoint/web_socket_propose'
-require 'alphapoint/get_products'
-
-require 'alphapoint/call/base'
-require 'alphapoint/call/get_products'
-
-
-
+	
 module Alphapoint
 
 	module Call
